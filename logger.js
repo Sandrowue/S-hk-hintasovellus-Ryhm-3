14897c49f5ef668f1d0568908488827e7b1b9402
SANDRO WUTHRICH <aleksi.w@protonmail.com>
2023-11-23 10:32:51 +0200

    logger.js kommentoitunut koodin poistaminen

--- logger.js
+++ logger.js
@@ -1,19 +1,5 @@
 const fs = require('fs');
 
-/*const timestamp = new Date();
-const isoTimeStamp = timestamp.toISOString();
-let operation = 'Data Fetch operation';
-let status = 'processes successfully';
-let entry = operation + ' ' + status + ' @ ' + isoTimeStamp + '\n'
-
-console.log(entry);
-
-fs.appendFile('dataOperation.log', entry, (err) => {
-    if (err) {
-        console.log(err);
-    }
-})*/
-
 const add2log = (entry, fileName) => {
     const isoTimeStamp = new Date().toISOString();
     const logRow = entry + '@' + isoTimeStamp + '\n'
